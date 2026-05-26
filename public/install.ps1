ed690eec98793e5cd64b87e65a3cd89ad7cdc9f7
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2026-05-26 07:46:39 +0100

    Release v0.0.77

--- public/install.ps1
+++ public/install.ps1
@@ -163,10 +163,8 @@ function Add-ToPath {
         return
     }
 
-    $currentUserPath = [Environment]::GetEnvironmentVariable("Path", "User")
-    if ($null -eq $currentUserPath) { $currentUserPath = "" }
-    $currentMachinePath = [Environment]::GetEnvironmentVariable("Path", "Machine")
-    if ($null -eq $currentMachinePath) { $currentMachinePath = "" }
+    $currentUserPath = [Environment]::GetEnvironmentVariable("Path", "User") ?? ""
+    $currentMachinePath = [Environment]::GetEnvironmentVariable("Path", "Machine") ?? ""
 
     if ($currentUserPath -like "*$BinDir*" -or $currentMachinePath -like "*$BinDir*") {
         return  # Already in PATH
